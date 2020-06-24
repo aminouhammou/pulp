fdb2e0765a45d69229ec838a9ba546b32b589218
aminouhammou <62098095+aminouhammou@users.noreply.github.com>
2020-06-24 16:42:51 +0200

    Update ILS.py

--- ILS.py
+++ ILS.py
@@ -1,173 +1,277 @@
-import numpy as np
-from pulp import *
 import pandas as pd
-import networkx as nx
+import numpy as np
+from itertools import combinations
+import math as mt
 import random
 
-#Lecture du tableau Excel
 if __name__ == "__main__":
 
-   InputData = "InputDataHubSmallInstance.xlsx"
+    InputData = "InputDataHubSmallInstance.xlsx"
 
-  # Input Data Preparation #
-   def read_excel_data(filename, sheet_name):
-      data = pd.read_excel(filename, sheet_name=sheet_name, header=None)
-      values = data.values
-      if min(values.shape) == 1:  # This If is to make the code insensitive to column-wise or row-wise expression #
-          if values.shape[0] == 1:
-              values = values.tolist()
-          else:
-              values = values.transpose()
-              values = values.tolist()
-          return values[0]
-      else:
-          data_dict = {}
-          if min(values.shape) == 2:  # For single-dimension parameters in Excel
-              if values.shape[0] == 2:
-                  for i in range(values.shape[1]):
-                      data_dict[i+1] = values[1][i]
-              else:
-                  for i in range(values.shape[0]):
-                      data_dict[i+1] = values[i][1]
+    def read_excel_data(filename, sheet_name):
+        data = pd.read_excel(filename, sheet_name=sheet_name, header=None)
+        values = data.values
+        if min(values.shape) == 1:  # This If is to make the code insensitive to column-wise or row-wise expression #
+            if values.shape[0] == 1:
+                values = values.tolist()
+            else:
+                values = values.transpose()
+                values = values.tolist()
+            return values[0]
+        else:
+            data_dict = {}
+            if min(values.shape) == 2:  # For single-dimension parameters in Excel
+                if values.shape[0] == 2:
+                    for i in range(values.shape[1]):
+                        data_dict[i + 1] = values[1][i]
+                else:
+                    for i in range(values.shape[0]):
+                        data_dict[i + 1] = values[i][1]
 
-          else:  # For two-dimension (matrix) parameters in Excel
-              for i in range(values.shape[0]):
-                  for j in range(values.shape[1]):
-                      data_dict[(i+1, j+1)] = values[i][j]
-          return data_dict
- 
-#This section reads the data from Excel#
+            else:  # For two-dimension (matrix) parameters in Excel
+                for i in range(values.shape[0]):
+                    for j in range(values.shape[1]):
+                        data_dict[(i + 1, j + 1)] = values[i][j]
+            return data_dict
 
-#Read a set (set is the name of the worksheet)
-   
-   NodeNum = read_excel_data(InputData, "NodeNum")
-   NodeNum = NodeNum[0]
-   print("NodeNum: ", NodeNum)
 
-   set = [i for i in range(NodeNum+1)]
+    nodeNum = read_excel_data(InputData, "NodeNum")
 
-   alpha = read_excel_data(InputData, "alpha")
-   alpha = alpha[0]
-   print("alpha: ", alpha)
+    flow_wij = read_excel_data(InputData, "flow(wij)")
 
-   flow_wij = read_excel_data(InputData, "flow(wij)")
-   print("flow(wij): ", flow_wij)
+    Cost_cij = read_excel_data(InputData, "varCost(cij)")
 
-   varCost_cij = read_excel_data(InputData, "varCost(cij)")
-   print("varCost(cij): ", varCost_cij)
+    fixCost_fk = read_excel_data(InputData, "fixCost(fk)")
 
-   fixCost_fk = read_excel_data(InputData, "fixCost(fk)")
-   print("fixCost(fk): ", fixCost_fk)
+    alpha = read_excel_data(InputData, "alpha")
 
-   Cap_ckmax = read_excel_data(InputData, "Cap(ckmax)")
-   print("Cap(ckmax): ", Cap_ckmax)
+    cap = read_excel_data(InputData, "Cap(ckmax)")
 
-#Solution initiale
 
-def network():
-    M = [] #matrice des couples 
-    L = [1,2,3,4,5,6,7,8] #liste des spokes
-    H = [] #liste des hubs
-    for k in range(3):
-        h = random.choice(L) #on récupère un chiffre au hasard dans la liste L
-        L.remove(h)
-        H.append(h)
+    def getVoisin(solution,noeud): 
+        voisins = []
+        for (i,j) in solution :
+            if (i == noeud) :
+                voisins.append(j)
+            if (j == noeud) : 
+                voisins.append(i)
+        return voisins
+
+    def getAllPathFromi_bis(solution,current,shortest,visited,path) :
+        voisins = getVoisin(solution,current)
+        visited.append(current)
+
+        path[current] = shortest 
+        for voisin in voisins :
+            if (voisin not in visited) :
+                getAllPathFromi_bis(solution,voisin, shortest + [voisin],visited,path)
     
-    for s in L: #on parcourt la liste des spokes non choisis comme hubs
-        h = random.choice(H) #on sélectionne un hub au hasard pour le relier au spoke k
-        M.append([h,s]) #on ajoute le couple hub/spoke
+    def getAllPathFromi(solution,i) :
+        path = {}
+        getAllPathFromi_bis(solution,i,[i],[],path)
+        return path
+
+    def verify(solution,hub) : 
+        hub_flow = {}
+        for i in hub :
+            hub_flow[i] = 0
+    
+        for i in range(1,nodeNum[0]+1) :
+            path_i = getAllPathFromi(solution,i)
+            for j in range(1,nodeNum[0]+1) :
+                if (i != j and j in path_i.keys()):
+                    chemin = path_i[j]
+                    for value in hub : 
+                        if (value in chemin) :
+                            hub_flow[value] += flow_wij[i,j]
+
+        for value in hub :
+            if hub_flow[value] > cap[value - 1] :
+                return False
+        return True
+
+
+    def findSolution2(N) :
+        cap_copy = cap.copy()
         
-    #on relie les hubs entre eux aléatoirement 
-    h = random.choice(H) 
-    H.remove(h)
-    M.append([h, H[0]])
-    M.append([h, H[1]])
-    H.append(h)
-    
-    return getGraph(M)
+        hub = []
+        node = [i + 1 for i in range(nodeNum[0])]
+        for i in range(N) :
+            hub_max = cap.index(max(cap_copy)) + 1
+            hub.append(hub_max)
+            node.remove(hub_max)
+            cap_copy.remove(max(cap_copy))
 
-def getGroup (M, p):
-    H = []
-    for m in M :
-        node = m[p]
-        H.append(node)
-    return H 
+        solution = []
+        arbred = [hub[0]]
 
-def getGraph(M):
-    M = []
-    H = getGroup(M,0) #hubs
-    M.append(H)
-    S = getGroup(M,1) #spokes
-    M.append(S) 
-    return M
+        for i in range(len(hub) - 1) : 
+            arbred_cop = arbred.copy()
+            while arbred_cop != [] :
+                choice = random.choice(arbred_cop) 
+                new_sol = solution.copy()
+                new_sol.append((choice,hub[i+1]))
+                if (verify(new_sol,hub)) :
+                    arbred.append(hub[i+1])
+                    solution.append((choice,hub[i+1]))
+                    break
+                arbred_cop.remove(choice)
+                
 
-print(network())
+        for i in node : 
+            hub_copy = hub.copy()
+            while hub_copy != [] :
+                choice = random.choice(hub_copy) 
+                new_sol = solution.copy()
+                new_sol.append((choice,i))
+                if (verify(new_sol,hub) == True) :
+                    solution.append((choice,i)) 
+                    break
+                hub_copy.remove(choice)
+
+        return hub,solution
+
+    #Nombre de hub
+    def findSolution(N):
+        hub = []
+        node = [i + 1 for i in range(nodeNum[0])]
+        for i in range(N) :
+            hub.append(random.choice(node))
+            node.remove(hub[i])
+
+        solution = []
+        arbred = [hub[0]]
+        for i in range(len(hub) - 1) : 
+            solution.append((random.choice(arbred),hub[i+1]))
+            arbred.append(hub[i+1])
+
+        for i in node :
+            solution.append((random.choice(hub),i))
+            #min = Cost_cij[(hub[0],i)]
+            #value = hub[0]
+            #for j in hub :
+            #    if (min > Cost_cij[(j,i)]) :
+            #        min = Cost_cij[(j,i)]
+            #        value = j
+            #solution.append((value,i))
+
+        return hub,solution
+
+    #def 
+    def objectiveValue(hub,solution): 
+        fk = 0
+        for i in hub : 
+            fk += fixCost_fk[i - 1]
+
+        var_cost = 0
+        for i in range(1,nodeNum[0]+1) :
+            path_i = getAllPathFromi(solution,i)
+            for j in range(1,nodeNum[0]+1) :
+                if (i != j):
+                    chemin = path_i[j]
+                    for m in range(len(chemin) - 1) :
+                        k = chemin[m]
+                        l = chemin[m+1]
+                        if (k in hub and l in hub):
+                            var_cost +=  alpha[0] * Cost_cij[(k,l)] * flow_wij[(i,j)]
+                        else : 
+                            var_cost += Cost_cij[(k,l)] * flow_wij[(i,j)]
+        return var_cost + fk
+
+    def local1(hub,solution) : 
+        old_spokes = []
+        for (i,j) in solution :
+            if j not in hub :
+                old_spokes.append(j)
+
+        i = random.randint(0,len(old_spokes))
+        start = old_spokes[0:i]
+        start.reverse() 
+        new_spokes = start + old_spokes[i:]
+        new_sol = []
+        for (i,j) in solution :
+            if j not in hub :
+                new_sol.append((i,new_spokes.pop(0)))
+            else :
+                new_sol.append((i,j))
+        return hub, new_sol
+
+    def getNewSol(solution,spoke,hub) : 
+        new_sol = []
+        for (h,s) in solution :
+            if s == spoke :
+                new_sol.append((hub,spoke))
+            else : 
+                new_sol.append((h,s))
+        return new_sol
+
+    def local2(hub,solution) : 
+        # Select one spoke
+        node = [i + 1 for i in range(nodeNum[0]) if i + 1 not in hub]
+        selected = random.choice(node)
+
+        # search hub that lead to minimum cost with that spoke
+        min_h = hub[0]
+        sol_0 = getNewSol(solution,selected,min_h)
+        value = objectiveValue(hub,sol_0)
+        for j in hub :
+            sol_j = getNewSol(solution,selected,j)
+            value_j = objectiveValue(hub,sol_j)
+            if (value_j < value) : 
+                min_h = j
+                value = value_j
+
+        return hub,getNewSol(solution,selected,min_h)
 
 
-def Hub_Spoke(M): #renvoie la liste des hubs et des spokes d'une matrice
-    H = []
-    S =[]
-    for i in M[0]:
-        H += [i]
-    for j in M[1]:
-        if j not in H :
-            S += [j]
-    return(H,S)
+    def shaking(hub, solution) :
+        index =  random.randrange(len(hub))
+        first = hub[index]
+        second = hub[(index + 1) % len(hub)]
 
-def Local_Search(M): #Algorithme du Local Search
-    H,S = Hub_Spoke(M)
-    for i in S : #on parcourt la liste des spokes pour leur appliquer le Local Search
-        for j in range (len(M[0])):
-            if M[j][1] == i:
-                index_hub = M[j][0] #on cherche l'index de l'hub qui correspond à ce spoke
-        S_final = cout_tot(M) #variable qui définit le coût du modèle qui sera retenu
-        S_current = 0 #variable qui définit le coût du modèle en cours
-        for k in H : #on parcourt la liste de hubs
-            M_current = [k for k in M] #on copie la matrice de notre modèle
-            for l in range (len(M[0])): #on parcourt la première colonne du tableau 
-                if M[l][0] != m[index_hub][0]: #on vérifie que l'index est le bon
-                    if M[l][0] == k:
-                        M_current[l][0] = k
-                        M_current[index_hub][0] = M[k][0] #on remplace tous les hubs
-            S_current = cout_tot(M_current) #on calcule le prix du modèle en cours
-            if S_current < S_final: 
-                S_final = S_current 
-                M = M_current 
-    return M
+        new_sol = []
+        for (h,s) in solution : 
+            if (h == second and s == first) or (h == first and s == second):
+                new_sol.append((s,h))
+            elif (h == first) :
+                new_sol.append((second,s))
+            elif (h == second) :
+                new_sol.append((first,s))
+            elif (s == first) :
+                new_sol.append((h,second))
+            elif (s == second):
+                new_sol.append((h,first))
+            else :
+                new_sol.append((h,s))
+        return hub,new_sol
 
-def Local_Search2(M): #Algorithme du Local Search 2
-    H,S = Hub_Spoke(M)
-    for i in S : #on parcourt la liste des spokes pour leur appliquer le Local Search 2
-        for j in range (len(M[0])):
-            if M[j][1] == i:
-                index_hub = M[j][1] #on cherche l'index de l'hub qui correspond à ce spoke
-        S_final = cout_tot(M) 
-        S_current = 0 
-        for k in spokes : #on parcourt la liste des spokes
-            M_current =[k for k in M]
-            for l in range (len(M[1])): #on parcourt la deuxième colonne du tableau
-                if M[l][1] != i: #on vérifie que l'index est le bon
-                    if M[l][1] == k:
-                        M_current[l][1] = i
-                        M_current[index_hub][1] = M[k][0] #on remplace tous les spokes
-            S_current = cout_tot(M_current) #on calcule le prix du modèle en cours
-            if S_current > S_final: 
-                S_final = S_current 
-                M = M_current 
-    return M
 
-            
-def cout_tot(M): #calcul du coût d'un modèle
-    H, S = Hub_Spoke(M)
-    Graph = nx.Graph(M)
-    All_Path = dict(nx.all_pairs_shortest_path(Graph)) #création des chemins les plus courts
-    cost = 0
-    for i in range (1,9):
-        cost+= fixCost_fk[i] #ajout des coûts fixes
-        for j in range (1,9):
-            for k in All_Path[(i, j)] :
-                if k!= i:
-                    cost+= flow_wij[(i,j)]*varCost_cij[(i, k)] #ajout des coûts variables en suivant le plus court chemin
-    return cost
-            
+
+
+N = 7
+hub, solut = findSolution(N)
+acc = 0
+while(verify(solut,hub) == False) :
+    hub, solut = findSolution(N)
+print("Première solution trouvé :")
+min_o = objectiveValue(hub,solut)
+print("coût : ",min_o)
+
+while(True) :
+    hub1 = []
+    solut1 = []
+    i = random.randint(1,3)
+    if i == 1 :
+        hub1,solut1 = local1(hub,solut)
+    elif i == 2 :
+        hub1,solut1 = local2(hub,solut)
+    else : 
+        hub1,solut1 = shaking(hub,solut)
+
+    verified = verify(solut1,hub1) 
+    min1 = objectiveValue(hub1,solut1)
+    if (verified == True and min_o > min1 ):
+        min_o = min1
+        hub,solut = hub1,solut1
+        print("coût : ",min_o)
             
